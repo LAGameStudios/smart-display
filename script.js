4e1dc13150803baee770e58911a116c3507521da
LAGameStudios <166259325+LAGameStudios@users.noreply.github.com>
2024-08-10 05:08:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -32,18 +32,32 @@ function changeCenterWidget() {
     const centerWidget = document.querySelector('.center-widget');
     const selector = document.getElementById('center-widget-selector').value;
 
-    // Clear current center widget
+    // Remove the current center widget if it exists
     if (centerWidget) {
         centerWidget.remove();
     }
 
-    // Add selected widget to center
+    // Add the selected widget to the center
     const container = document.querySelector('.container');
     if (selector) {
         const newWidget = document.createElement('div');
         newWidget.className = 'center-widget';
         newWidget.id = `${selector}-widget`;
-        newWidget.innerHTML = `<p>${selector.charAt(0).toUpperCase() + selector.slice(1)} Widget</p>`;
+        
+        switch (selector) {
+            case 'spotify':
+                newWidget.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/33pt9HBdGlAbRGBHQgsZsU?utm_source=generator" width="100%" height="280" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+                break;
+            case 'youtube':
+                newWidget.innerHTML = `<p>YouTube widget content here.</p>`; // Replace with desired content
+                break;
+            case 'todo':
+                newWidget.innerHTML = document.getElementById('todo-list').innerHTML;
+                break;
+            default:
+                newWidget.innerHTML = `<p>Blank space</p>`;
+                break;
+        }
         container.appendChild(newWidget);
     }
 }
